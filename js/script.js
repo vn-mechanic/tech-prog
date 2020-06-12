cfd9ea2be39b2612e4ae1bc6aedd4664bde2e0af
d15hw45h3r <vinnichenkovaleriaa@gmail.com>
2020-06-12 14:24:05 +0300

    Added second page

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function clickFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("mLink");
+
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
